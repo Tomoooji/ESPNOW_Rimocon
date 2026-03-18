b051827745d7055aed91c0a5e35416d3cd95946a
Tomoooji <tomoji.k.2nd@gmail.com>
2026-03-18 10:13:13 +0900

    inital version

---
+++ espnow_rimocon_recv/espnow_rimocon_recv.ino
@@ -0,0 +1,33 @@
+#include "Rimocon_ESPNOW.h"
+
+#pragma pack(puch, 1)
+struct message{
+  bool left, right;
+  int num;
+};
+#pragma pack(pop)
+
+ESPnowRimocon<message> rimocon;
+
+void setup(){
+  Serial.begin(115200);
+
+  if(!rimocon.init())return;
+
+  pinMode(2,OUTPUT);
+  pinMode(17,OUTPUT);
+  pinMode(16,OUTPUT);
+}
+
+void loop(){
+  digitalWrite(2,rimocon.receive_new);
+  if(rimocon.receive_new){
+    rimocon.receive_new = false;
+    Serial.println(rimocon.received.num);
+    digitalWrite(17, rimocon.received.left);
+    digitalWrite(16, rimocon.received.right);
+  }
+}
+//68:25:dd:32:12:50
+//Serial.println("success reciving");
+//Serial.println(getlatest);
---
+++ espnow_rimocon_send/espnow_rimocon_send.ino
@@ -0,0 +1,44 @@
+#include "Rimocon_ESPNOW.h"
+
+//constexpr 
+uint8_t receiverAddress[] = {0x68,0x25,0xdd,0x32,0x12,0x50};//{0x80, 0xf3, 0xda, 0x41, 0x47, 0xe8};//
+
+#pragma pack(puch, 1)
+struct message{
+  bool left, right;
+  int num;
+};
+#pragma pack(pop)
+
+ESPnowRimocon<message> rimocon(receiverAddress);
+
+void setup(){
+  Serial.begin(115200);
+
+  if(!rimocon.init())return;
+
+  pinMode(21, INPUT_PULLUP);
+  pinMode(19, INPUT_PULLUP);
+}
+
+void loop(){
+  rimocon.sending.left = !digitalRead(21);
+  rimocon.sending.right = !digitalRead(19);
+  rimocon.sending.num += rimocon.sending.num<100? 1: -100;
+
+  rimocon.send();
+  delay(1);
+
+  if(rimocon.success_send){
+    Serial.println(rimocon.sending.num);
+  }else{
+    Serial.println("....");
+  }
+
+}
+
+//Serial.println("reraly successed!");
+//Serial.println("maybe failed");
+//Serial.println("failed initialize");
+//Serial.println("failed peering");
+//Serial.println("failed sending");
